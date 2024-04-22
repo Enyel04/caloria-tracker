3bf5eedb08a85f6160e2b22118b06cc7682ffc76
Angel Mendez <110472083+Enyel04@users.noreply.github.com>
2024-04-22 11:53:10 -0400

    agregando useState para seguir lo que se esta llenando en el formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,59 @@
+import { useState } from "react"
 import { categories } from "../data/categoria"
+//Se importa data Categorias, haciendo alucion a que se esta llamando la data base
 
 export default function Form() {
-  return (
-    
 
+  //Se crea UseState para establecer lo que se validara en el formulario, sus datos inicales
+  
+  const [activity, setActivity] =useState({
+    category:1,
+    name:'',
+    calories:0,
+  })
+//Se crea el handleChange para sincronizar el formulario con la base de datos
+//se agrega como metodo "e" para añadir un atributo e irlo sincronizando
+//Saber la informacion, se puede de que tipo de dato es al colocarlo dentro de una parte de los llamados
+//Es decir, donde yo este llamando dicha funcion, o arrow function
+//Se llamada el select  e input
+
+  const handleChange= (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+    
+    setActivity({
+
+      ...activity,
+      [e.target.id]:e.target.value
+    })
+  
+    
+    
+    
+  
+    
+  }
+
+  return (
+  
     <form 
     
     className=" space-y-5 bg-white shadow p-10 rounded-lg"
     > 
+    
         <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className=" font-bold">Categorias:</label>
                 <select 
                 className=" border border-slate-300 p-2 rounded-lg bg-white"
                 id="category"
+                value={activity.category}
+                onChange={handleChange}
+           
                 >
                   {categories.map(category => (
 
                     <option 
                     key={category.id}
                     value={category.id}>
+
                         { category.name}
                     </option>
                 
@@ -29,12 +64,14 @@ export default function Form() {
                 </select>
 
                 <div className="grid grid-cols-1 gap-3">
-                <label  htmlFor="activity" className=" font-bold">Actividad:</label>
+                <label  htmlFor="name" className=" font-bold">Actividad:</label>
 
                     <input type="text" 
-                    id="activity"
+                    id="name"
                     className=" border border-slate-300 p-2 rounded-lg"
                     placeholder="Ejemplo, comida"
+                  value={activity.name}
+                    onChange={handleChange}
                     /> {/*Final Form  */}
                 
 
@@ -47,6 +84,8 @@ export default function Form() {
                         id="calories"
                         className=" border border-slate-300 p-2 rounded-lg"
                         placeholder="Calorias"
+                        value={activity.calories}
+                        onChange={handleChange}
                     /> {/*Final Form  */}
                 </div>
         </div>
